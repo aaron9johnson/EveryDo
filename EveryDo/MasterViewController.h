acc4d25fb523a7b35073fe15fa608626c3357d79
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-17 11:16:36 -0700

    Initial Commit

---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Aaron Johnson on 2017-10-17.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
